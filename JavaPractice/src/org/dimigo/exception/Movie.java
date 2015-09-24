5cfee42736014db94d9e70850c73df8838f70449
SangB <SangB@SangB-PC>
2015-09-24 11:03:24 +0900

    .

---
+++ JavaPractice/src/org/dimigo/exception/Movie.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package org.dimigo.exception;
+
+/**
+ * <pre>
+ * org.dimigo.exception
+ *   │_ Movie
+ *
+ * 1. 개요 :
+ * 2. 작성일 : 2015. 9. 23.
+ * </pre>
+ * 
+ * @author		: 이상빈
+ * @version		: 1.0
+ */
+public class Movie{
+
+	private String title;
+	private int limitAge;
+	
+	public Movie() {}
+	
+	public Movie(String title, int limitAge) {
+		this.title = title;
+		this.limitAge = limitAge;
+	}
+
+	/**
+	 * @return the title
+	 */
+	public String getTitle() {
+		return title;
+	}
+
+	/**
+	 * @return the limitAge
+	 */
+	public int getLimitAge() {
+		return limitAge;
+	}
+	
+	
+}
