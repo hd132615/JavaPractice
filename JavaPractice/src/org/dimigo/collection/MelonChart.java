c051e3f9d5e617d9f915f8339e0f0d646fa07f36
SangB <SangB@SangB-PC>
2015-10-01 16:39:41 +0900

    실습과제 5

--- JavaPractice/src/org/dimigo/collection/MelonChart.java
+++ JavaPractice/src/org/dimigo/collection/MelonChart.java
@@ -27,26 +27,26 @@ public class MelonChart {
 		list.add(new Music("보통연애", "박경"));
 		list.add(new Music("취향저격", "iKON"));
 		
-		System.out.println("-- << 멜론 챠트 >> --");
+		System.out.println("---- << 멜론 챠트 >> ----");
 		printList(list);
 		System.out.println();
 		
-		System.out.println("-- << 2위 곡 추가 >> --");
+		System.out.println("---- << 2위 곡 추가 >> ----");
 		list.add(1, new Music("레옹", "이유갓지"));
 		printList(list);
 		System.out.println();
 		
-		System.out.println("-- << 3위 곡 변경 >> --");
+		System.out.println("---- << 3위 곡 변경 >> ----");
 		list.set(2, new Music("맙소사", "황태지"));
 		printList(list);
 		System.out.println();
 		
-		System.out.println("-- << 4위 곡 삭제 >>--");
+		System.out.println("---- << 4위 곡 삭제 >> ----");
 		list.remove(3);
 		printList(list);
 		System.out.println();
 	
-		System.out.println("-- << 전체 리스트 삭제 >>--");
+		System.out.println("---- << 전체 리스트 삭제 >> ----");
 		list.clear();
 		printList(list);
 	}
