ac328da0f5bb759905b59db96b69630edb162de6
SangB <SangB@SangB-PC>
2015-11-05 11:23:28 +0900

    실습과제 9

---
+++ JavaPractice/src/org/dimigo/thread/Race.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package org.dimigo.thread;
+
+/**
+ * <pre>
+ * org.dimigo.thread
+ *   │_ Race
+ *
+ * 1. 개요 :
+ * 2. 작성일 : 2015. 11. 5.
+ * </pre>
+ * 
+ * @author		: 이상빈
+ * @version		: 1.0
+ */
+public class Race {
+	
+	public static void main(String[] args) {
+		System.out.println("main thread start");
+		
+		Runner r1 = new Runner("A");
+		Runner r2 = new Runner("B");
+	
+		r1.start();
+		r2.start();
+	
+		System.out.println("main thread end");
+	}
+}
