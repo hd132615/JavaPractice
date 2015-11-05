132551fe759ffc45ee5d6c2d38b2480071c6b41c
SangB <SangB@SangB-PC>
2015-11-05 11:47:59 +0900

    실습과제 10

---
+++ JavaPractice/src/org/dimigo/thread/Runner2.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package org.dimigo.thread;
+
+/**
+ * <pre>
+ * org.dimigo.thread
+ *   │_ Runner
+ *
+ * 1. 개요 :
+ * 2. 작성일 : 2015. 11. 5.
+ * </pre>
+ * 
+ * @author		: 이상빈
+ * @version		: 1.0
+ */
+public class Runner2 implements Runnable{
+	
+	private String name;
+
+	public Runner2() {}
+	
+	/**
+	 * @param name
+	 */
+	public Runner2(String name) {
+		super();
+		this.name = name;
+	}
+	
+	@Override
+	public void run() {
+		System.out.println(name + " 출발");
+		
+		for(int i = 10; i >= 0; i--) {
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			System.out.println(name + " " + i*10 + " 미터");
+		}
+		
+		System.out.println(name + " 골인");
+	}
+}
